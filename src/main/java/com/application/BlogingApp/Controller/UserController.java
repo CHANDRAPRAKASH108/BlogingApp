218117a59390c35b14ca16c232b46fd1c61f0f40
Chandra Prakash Kumar <chandraprakash.bhr@gmail.com>
2023-02-05 22:09:51 +0530

    added CRUD Operation for Category and also added post method for POST

--- src/main/java/com/application/BlogingApp/Controller/UserController.java
+++ src/main/java/com/application/BlogingApp/Controller/UserController.java
@@ -1,11 +1,17 @@
 package com.application.BlogingApp.Controller;
 
+import com.application.BlogingApp.Entity.User;
+import com.application.BlogingApp.Payloads.ApiResponse;
 import com.application.BlogingApp.Payloads.UserDto;
 import com.application.BlogingApp.Service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/users")
@@ -16,8 +22,29 @@ public class UserController {
 
     //POST - Create User
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
         UserDto createUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto,@PathVariable Integer userId){
+        UserDto updateUserDto = this.userService.updateUser(userDto,userId);
+        return new ResponseEntity<>(updateUserDto,HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Integer userId){
+        UserDto getUser = this.userService.getOneUser(userId);
+        return new ResponseEntity<>(getUser,HttpStatus.OK);
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUser(){
+        return new ResponseEntity<>(this.userService.getAllUsers(),HttpStatus.OK);
+    }
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId){
+        this.userService.deleteUser(userId);
+        return new ResponseEntity<>(new ApiResponse("User deleted successfully",true), HttpStatus.OK);
     }
 }
